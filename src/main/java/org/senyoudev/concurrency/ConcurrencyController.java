5f39675642c1b453910c125a4cf0aa16128b95c5
senyoudev <younesmeskafe2018@gmail.com>
2024-10-11 01:10:07 +0100

    docs(ConcurrencyController): Adding JavaDoc

--- src/main/java/org/senyoudev/concurrency/ConcurrencyController.java
+++ src/main/java/org/senyoudev/concurrency/ConcurrencyController.java
@@ -15,6 +15,12 @@ import java.util.function.Supplier;
 public class ConcurrencyController {
     private final ReadWriteLock rwLock = new ReentrantReadWriteLock(true); // Fairness policy which means that the longest waiting thread gets the lock
 
+    /**
+     * This method is used to read from the cache.
+     * It acquires the read lock before reading from the cache and releases the read lock after reading from the cache.
+     * @param readOperation It's a supplier which is a functional interface that takes no arguments and returns a result.
+     *                      which means we pass to it a function that will get executed and return a result.
+     */
     public <T> T read(Supplier<T> readOperation) {
         rwLock.readLock().lock();
         try {
@@ -24,6 +30,12 @@ public class ConcurrencyController {
         }
     }
 
+    /**
+     * This method is used to write to the cache.
+     * It acquires the write lock before writing to the cache and releases the write lock after writing to the cache.
+     * @param writeOperation It's a runnable which is a functional interface that takes no arguments and returns no result.
+     *                       which means we pass to it a function that will get executed without returning a result.
+     */
     public void write(Runnable writeOperation) {
         rwLock.writeLock().lock();
         try {
@@ -33,6 +45,12 @@ public class ConcurrencyController {
         }
     }
 
+    /**
+     * This method is used to write to the cache and return a result.
+     * It acquires the write lock before writing to the cache and releases the write lock after writing to the cache.
+     * @param writeOperation It's a supplier which is a functional interface that takes no arguments and returns a result.
+     *                      which means we pass to it a function that will get executed and return a result.
+     */
     public <T> T writeWithResult(Supplier<T> writeOperation) {
         rwLock.writeLock().lock();
         try {
