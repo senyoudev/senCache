c4e6d0253eef5077d676895fe5fb5988895d5e3f
senyoudev <younesmeskafe2018@gmail.com>
2024-10-11 17:40:45 +0100

    feat: Cache store/In memory

---
+++ src/main/java/org/senyoudev/core/CacheStore.java
@@ -0,0 +1,51 @@
+package org.senyoudev.core;
+
+import org.senyoudev.eviction.EvictionPolicy;
+
+import java.util.Map;
+import java.util.Objects;
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * This class is our in-memory cache store.
+ */
+public class CacheStore<K,V> {
+    private final Map<K,V> store;
+    private final EvictionPolicy<K> evictionPolicy;
+    private final int maxSize;
+
+
+    public CacheStore(EvictionPolicy<K> evictionPolicy, int maxSize) {
+        if(maxSize <= 0) {
+            throw new IllegalArgumentException("Max Size must be positive");
+        }
+        this.store = new ConcurrentHashMap<>(maxSize);
+        this.evictionPolicy = evictionPolicy;
+        this.maxSize = maxSize;
+    }
+
+    public V get(K key) {
+        Objects.requireNonNull(key, "Key cannot be null");
+        V value = store.get(key);
+        if(value != null) {
+            evictionPolicy.onAccess(key);
+        }
+        return value;
+    }
+
+    public void put(K key, V value) {
+        Objects.requireNonNull(key, "Key cannot be null");
+        Objects.requireNonNull(value, "Value cannot be null");
+        if(store.size() >= maxSize && !store.containsKey(key)) {
+            evictionPolicy.evict();
+        }
+        store.put(key,value);
+        evictionPolicy.onPut(key);
+    }
+
+
+
+
+
+
+}
