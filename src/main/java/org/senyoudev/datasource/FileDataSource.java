38431a8fcc5dedf6f2b7283e0ee8d0badea0afb6
senyoudev <younesmeskafe2018@gmail.com>
2024-10-12 01:52:56 +0100

    feat: Implement clear in File Data Source

--- src/main/java/org/senyoudev/datasource/FileDataSource.java
+++ src/main/java/org/senyoudev/datasource/FileDataSource.java
@@ -1,8 +1,22 @@
 package org.senyoudev.datasource;
 
-public record FileDataSource() implements DataSource {
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Path;
+
+public record FileDataSource(Path filePath) implements DataSource {
+
   @Override
   public String getType() {
     return "FILE";
+  }
+
+  @Override
+  public void clear() {
+    try (FileWriter fileWriter = new FileWriter(filePath.toFile())) {
+      fileWriter.write("");
+    } catch (IOException e) {
+      throw new RuntimeException("Error clearing file", e);
+    }
   }
 }
